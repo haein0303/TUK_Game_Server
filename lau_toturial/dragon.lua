8c066021b0e2e14eedd8ceaaa5383667861f60b9
NERU <2849593@naver.com>
2023-05-16 11:33:41 +0900

    add_lua

---
+++ lau_toturial/dragon.lua
@@ -0,0 +1,12 @@
+function plustwo(x)
+local a;
+a = 2
+return x+a;
+end
+
+pos_x = 6;
+pos_y = plustwo(pos_x)
+
+function addnum( a,b )
+return  c_addnum(a,b)
+end
--- lau_toturial/lua.cpp
+++ lau_toturial/lua.cpp
@@ -5,21 +5,58 @@
 
 using namespace std;
 
+int c_addnum(lua_State* L) {
+	int a = lua_tonumber(L, -2);
+	int b = lua_tonumber(L, -1);
+	lua_pop(L, 3);
 
+	lua_pushnumber(L, a + b);
+	return 1;
+}
 
 int main() {
-	const char lua_pro[] = "print \"hello\"\n";
-
-	//인터프리터를 사용하기 위해서 가상 머신을 사용
+	
 	lua_State* L = luaL_newstate();
 	//표준 라이브러리 로딩
 	luaL_openlibs(L);
 
-	luaL_loadbuffer(L,lua_pro,strlen(lua_pro),"Line");
+	luaL_loadfile(L, "dragon.lua");
 	
 	int err = lua_pcall(L, 0, 0, 0);
 	if (err) {
 		cout << "ERROR : " << lua_tostring(L,-1) << endl;
 	}
+
+	//C 함수 등록
+	lua_register(L, "c_addnum", c_addnum);
+	//루아 함수 부르기
+	lua_getglobal(L, "addnum");
+	//스텍에 값 넣어두기
+	lua_pushnumber(L, 100);
+	lua_pushnumber(L, 200);
+	//함수 호출스
+	lua_pcall(L, 2, 1, 0);
+	//결과 받고 (가상머신 스텍에 있는거 받아오기)
+	int sum = lua_tonumber(L, -1);
+	//스텍 깔끔하게 비우기
+	lua_pop(L, 1);
+
+	cout << "sum : " << sum << endl;
+	/*lua_getglobal(L, "plustwo");
+
+
+	lua_pushnumber(L, 100);
+	lua_pcall(L, 1, 1, 0);
+	int result = lua_tonumber(L, -1);
+	lua_pop(L,1);
+
+	cout <<"result : " << result << endl;
+	lua_getglobal(L, "pos_x");
+	lua_getglobal(L, "pos_y");
+	int pos_x = lua_tonumber(L, -2);
+	int pos_y = lua_tonumber(L, -1);
+	lua_pop(L, 2);
+
+	cout << "X : " << pos_x << " Y : " << pos_y << endl;*/
 	lua_close(L);
 }
